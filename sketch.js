f9e44f6cbb0eff4e68f48bbc738eaa36d640f4a9
Arnav891 <73640818+Arnav891@users.noreply.github.com>
2020-10-29 15:25:10 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -77,9 +77,9 @@ function draw() {
     score = score + Math.round(getFrameRate()/60);
     ground.velocityX = -(6 + 3*score/100);
   
-    //if(keyDown("space") && trex.y >= 159) {
-     // trex.velocityY = -12;
-    //}
+    if(keyDown("space") && trex.y >= 159) {
+      trex.velocityY = -12;
+    }
   
     trex.velocityY = trex.velocityY + 0.8
   
